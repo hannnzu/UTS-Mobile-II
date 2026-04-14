f3a5187e739d182e03fdd1818ab307c02cb9ed22
hannnzu <zyrexxzz@gmail.com>
2026-04-14 19:03:16 +0700

    menambahkan interface GuessDao untuk operasi room database pada GameUiState

--- app/src/main/java/com/example/unscramble/data/GuessDao.kt
+++ app/src/main/java/com/example/unscramble/data/GuessDao.kt
@@ -1,13 +1,19 @@
 package com.example.unscramble.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.unscramble.ui.GameUiState
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GuessDao {
 
     @Query("SELECT * FROM game_ui_state")
-    fun getAll(): List<GameUiState>
+    fun getAll(): Flow<List<GameUiState>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(gameState: GameUiState)
 
 }
