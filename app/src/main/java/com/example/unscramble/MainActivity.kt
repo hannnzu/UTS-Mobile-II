4f5de033d2b13a0cf70034737d8380e5f6e894b7
hannnzu <zyrexxzz@gmail.com>
2026-04-14 15:08:48 +0700

    membuat instance baru pada MainActivity untuk database

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -23,12 +23,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.room.Room
+import com.example.unscramble.ui.AppDatabase
 import com.example.unscramble.ui.GameScreen
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "db_potter"
+        ).fallbackToDestructiveMigration(true)
+            .build()
+
         super.onCreate(savedInstanceState)
         setContent {
             UnscrambleTheme {
